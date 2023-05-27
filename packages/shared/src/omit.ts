b7bcfdb7de6bee03b4845704db9637115382c850
yangtianxia <1304082170@qq.com>
2023-05-27 15:59:18 +0800

    Update @txjs/shared `omit`

--- packages/shared/src/omit.ts
+++ packages/shared/src/omit.ts
@@ -9,15 +9,14 @@
  * // => { b: 2 }
  * ```
  */
-export function omit<T, K extends keyof T>(
-  target: T,
-  keys: ReadonlyArray<K>
-) {
+export function omit<T, K extends keyof T>(target: T, keys: ReadonlyArray<K>) {
   const shallowCopy = Object.assign({}, target)
   let i = keys.length
+
   while (i--) {
     const key = keys[i]
     delete shallowCopy[key]
   }
+
   return shallowCopy as Omit<T, K>
 }

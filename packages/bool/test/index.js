7a97bc1e0d8811d7fc723f6697e56e8e0dc246fb
yangtianxia <1304082170@qq.com>
2023-08-04 11:26:41 +0800

    Update @txjs/bool add test

---
+++ packages/bool/test/index.js
@@ -0,0 +1,12 @@
+const bool = require('../dist/index.cjs')
+
+console.log(bool.isEqual(12, 1))
+console.log(bool.isEqual(false, true))
+console.log(bool.isEqual([], []))
+console.log(bool.isEqual([0, {}, []], [0, {}, []]))
+console.log(bool.isEqual({}, {}))
+console.log(bool.isEqual({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 3 } }))
+console.log(bool.isEqual(new Date('2013-09-06'), new Date('2023-09-06')))
+console.log(bool.isEqual(new Date('2013-09-06'), new Date('2013-09-06')))
+console.log(bool.isEqual(/^\s+/, /^\s+/))
+console.log(bool.isEqual(/^\s+/, /^\S+/))

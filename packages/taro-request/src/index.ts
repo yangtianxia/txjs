1a91543a16399b6ea8e5b7d476327ebdce850575
yangtianxia <1304082170@qq.com>
2023-08-23 16:38:56 +0800

    Remove @txjs/taro-request

--- packages/taro-request/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Http } from './request/http'
-
-export { CancelToken, type ICancelTokenSource } from 'miniprogram-cancel-token'
-export * from './life-cycle'
-export * from './request/http'
-export * from './request/transform'
-
-export const REQUEST = new Http()
--- packages/taro-request/src/life-cycle/index.ts
+++
@@ -1,3 +0,0 @@
-export type { BaseConfiguration, ExtraConfiguration, SuccessParam } from './configuration'
-export { Listeners } from './listeners'
-export { LifeCycle } from './life-cycle'

4a7212b83cc65f15ee0fafa149932894252724e1
yangtianxia <1304082170@qq.com>
2023-09-13 14:07:08 +0800

    Update @txjs/bool isURL

--- packages/bool/src/isURL.ts
+++ packages/bool/src/isURL.ts
@@ -1,6 +1,5 @@
 import { isNil } from './isNil'
 
-// eslint-disable-next-line
 const reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
 
 /**

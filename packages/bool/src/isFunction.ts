6ccf7400a9b31b4dbe8522a7255a606c213ce4a9
yangtianxia <1304082170@qq.com>
2023-09-13 14:06:33 +0800

    Update @txjs/bool isFunction

--- packages/bool/src/isFunction.ts
+++ packages/bool/src/isFunction.ts
@@ -13,7 +13,6 @@ import { is } from './is'
  * // => false
  * ```
  */
-// eslint-disable-next-line
 export function isFunction<T>(value: T): value is T extends Function ? T : never {
   return is(value, 'Function')
 }

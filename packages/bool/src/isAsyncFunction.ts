fc211e4319c28c0731869aa1697b6c515127a8e8
yangtianxia <1304082170@qq.com>
2023-09-13 14:09:25 +0800

    Update @txjs/bool add isAsyncFunction

---
+++ packages/bool/src/isAsyncFunction.ts
@@ -0,0 +1,21 @@
+import { is } from './is'
+
+/**
+ * 检查 `value` 是否是 `async function` 类型
+ *
+ * @example
+ * ```ts
+ * const func = async () => {}
+ * const func1 = () => {}
+ *
+ * isAsyncFunction(func)
+ * // => true
+ * isAsyncFunction(func1)
+ * // => false
+ * isAsyncFunction(null)
+ * // => false
+ * ```
+ */
+export function isAsyncFunction<T>(value: T): value is T extends Function ? T : never {
+  return is(value, 'AsyncFunction')
+}

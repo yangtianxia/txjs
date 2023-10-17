a56aa7dd9a28dec5329a5a32cb2f2f6057856951
yangtianxia <1304082170@qq.com>
2023-10-17 17:44:29 +0800

    Feat @txjs/shared cloneDeep

---
+++ packages/shared/src/cloneDeep.ts
@@ -0,0 +1,38 @@
+import clone from 'shallow-clone'
+import { notNil, isFunction, isPlainObject, isArray } from '@txjs/bool'
+
+type InstanceClone<T> = true | ((value: T) => T)
+
+function cloneDeep<T>(value: T, instanceClone?: InstanceClone<T>) {
+	if (isPlainObject(value)) {
+		return cloneObjectDeep(value, instanceClone)
+	}
+	if (isArray(value)) {
+		return cloneArrayDeep(value, instanceClone)
+	}
+	return clone(value)
+}
+
+function cloneObjectDeep<T>(value: any, instanceClone?: InstanceClone<T>) {
+	if (isFunction(instanceClone)) {
+		return instanceClone(value)
+	}
+	if (instanceClone || isPlainObject(value)) {
+		const shallowCopy = notNil(value.constructor) ? new value.constructor() : Object.create(null)
+		for (let key in value) {
+			shallowCopy[key] = cloneDeep(value[key], instanceClone)
+		}
+		return shallowCopy
+	}
+	return value
+}
+
+function cloneArrayDeep<T>(value: any, instanceClone?: InstanceClone<T>) {
+	const shallowCopy = new value.constructor(value.length)
+	for (let i = 0, len = value.length; i < len; i++) {
+		shallowCopy[i] = cloneDeep(value[i], instanceClone)
+	}
+	return shallowCopy
+}
+
+export { cloneDeep }

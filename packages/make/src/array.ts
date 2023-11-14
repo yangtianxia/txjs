00bcdf687e4dcc9f3d0aace4f4c5fab54c865778
yangtianxia <1304082170@qq.com>
2023-11-14 10:26:28 +0800

    Update @txjs/make

--- packages/make/src/array.ts
+++ packages/make/src/array.ts
@@ -1,5 +1,5 @@
-export function makeArray<T = any>(): T[] | undefined
-export function makeArray<T = any>(defaultArr: T[]): T[]
-export function makeArray<T = any>(defaultArr?: T[]) {
+export function makeArray<T extends any>(): T[] | undefined
+export function makeArray<T extends any>(defaultArr: T[]): T[]
+export function makeArray<T extends any>(defaultArr: T[] | undefined = void 0) {
   return defaultArr
 }
--- packages/make/src/numeric.ts
+++ packages/make/src/numeric.ts
@@ -2,6 +2,6 @@ type Numeric = number | string
 
 export function makeNumeric(): Numeric | undefined
 export function makeNumeric(defaultVal: Numeric): Numeric
-export function makeNumeric(defaultVal?: Numeric) {
+export function makeNumeric(defaultVal: Numeric | undefined = void 0) {
   return defaultVal
 }

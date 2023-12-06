249960345b11d41e870b09e9b074c01b4b538acd
yangtianxia <1304082170@qq.com>
2023-12-06 15:18:43 +0800

    Feat(shared): 新增 toFixed 方法

---
+++ packages/shared/src/toFixed.ts
@@ -0,0 +1,20 @@
+/**
+ * toFixed
+ *
+ * @example
+ * ```ts
+ * toFixed(100)
+ * // => 100
+ * toFixed(100, 2)
+ * // => 100
+ * toFixed(3.1415926)
+ * // => 3
+ * toFixed(3.1415926, 2)
+ * // => 3.14
+ * ```
+ */
+export function toFixed(num: number, precision = 0) {
+	const multiplier = Math.pow(10, precision)
+	const wholeNum = Math.floor(num * multiplier)
+	return wholeNum / multiplier
+}

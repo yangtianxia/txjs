527b2e3fd62fddf0a998c725a4d159431524dbf3
yangtianxia <1304082170@qq.com>
2024-12-17 13:48:52 +0800

    feat(bool) 新增isHttpUrl方法

---
+++ packages/bool/src/isHttpUrl.ts
@@ -0,0 +1,18 @@
+const HTTP_URL_REGEX = /^(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()!@:%_+.~#?&//=]*))$/
+
+/**
+ * 检查 `value` 是否是以 `http` 或 `https` 开头的url
+ *
+ * @example
+ * ```ts
+ * isHttpUrl('https://www.example.com')
+ * // => true
+ * isHttpUrl('www.example.com')
+ * // => false
+ * isHttpUrl('foo/bar')
+ * // => false
+ * ```
+ */
+export function isHttpUrl(value: unknown): value is string {
+	return typeof value === 'string' && HTTP_URL_REGEX.test(value)
+}

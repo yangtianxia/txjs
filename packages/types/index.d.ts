20e41712c77f82983d4db48d0fd03398f745c8ab
yangtianxia <1304082170@qq.com>
2025-06-05 16:56:03 +0800

    chore(types) 声明调整为导出模式

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,25 +1,21 @@
-declare global {
-	type Numeric = number | string
+export type Numeric = number | string
 
-	type UnknownCallback<T = unknown, U = void> = (...args: T[]) => U
+export type UnknownCallback<T = unknown, U = void> = (...args: T[]) => U
 
-	type Writeable<T> = {
-    -readonly [P in keyof T]: T[P]
-  }
-
-	type NonNullableProps<T> = {
-    [p in keyof T]: NonNullable<T[p]>
-  }
-
-	type NonNullableParams<T> = T extends (...args: infer P) => infer R
-    ? (...args: { [K in keyof P]-?: NonNullable<P[K]> }) => R
-    : never
-
-	type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
-    ? S2 extends Uncapitalize<S2>
-      ? `${Uncapitalize<S1>}${KebabCase<S2>}`
-      : `${Uncapitalize<S1>}-${KebabCase<S2>}`
-    : S
+export type Writeable<T> = {
+  -readonly [P in keyof T]: T[P]
 }
 
-export {}
+export type NonNullableProps<T> = {
+  [p in keyof T]: NonNullable<T[p]>
+}
+
+export type NonNullableParams<T> = T extends (...args: infer P) => infer R
+  ? (...args: { [K in keyof P]-?: NonNullable<P[K]> }) => R
+  : never
+
+export type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S
